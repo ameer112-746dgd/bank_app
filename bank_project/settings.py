adad42f6a7e5fe012698a9e8a11e175370f919a5
ola <atobateleameer@gmail.com>
2024-11-26 17:42:10 +0100

    added vercel.json file

--- bank_project/settings.py
+++ bank_project/settings.py
@@ -26,9 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bank-app-6aor.onrender.com', 'localhost', '127.0.0.1']
-
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['bank-app-dusky-three.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition

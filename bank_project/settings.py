75f1120ac97d5244bf4dba3181bc1f462e054d2e
ola <atobateleameer@gmail.com>
2024-11-26 18:30:29 +0100

    allowhost-changed

--- bank_project/settings.py
+++ bank_project/settings.py
@@ -26,9 +26,11 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', 'bank-app-s9yz.vercel.app', 'localhost', '127.0.0.1']
-
-
+ALLOWED_HOSTS = [
+    os.getenv('DEPLOYMENT_URL', 'localhost'),
+    'localhost',
+    '127.0.0.1',
+]
 
 # Application definition
 

768df5a6427e64e76c8e12e0806c2abe559a4637
ola <atobateleameer@gmail.com>
2024-11-27 10:51:52 +0100

    'new4'

--- bank_project/settings.py
+++ bank_project/settings.py
@@ -24,11 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['bank-app-7ank.onrender.com', 'localhost', '127.0.0.1']
+# ALLOWED_HOSTS = ['bank-app-7ank.onrender.com', 'localhost', '127.0.0.1']
 
 ALLOWED_HOSTS = ['*.onrender.com', 'localhost', '127.0.0.1']
+
 
 # Application definition
 

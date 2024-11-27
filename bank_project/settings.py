00b797c5d911e6b083217ace072bfb4ea294f7f2
ola <atobateleameer@gmail.com>
2024-11-27 15:07:09 +0100

    new19

--- bank_project/settings.py
+++ bank_project/settings.py
@@ -26,8 +26,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+ALLOWED_HOSTS = ['bank-app-kqh5.onrender.com']
 
-ALLOWED_HOSTS = ['bank-app-syw5.onrender.com', 'localhost', '127.0.0.1']
+# ALLOWED_HOSTS = ['bank-app-syw5.onrender.com', 'localhost', '127.0.0.1']
 
 
 

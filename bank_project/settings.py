74af67bbbe2218039a2eaaa9a8d80d3db9b8bfc4
ola <atobateleameer@gmail.com>
2024-11-27 10:19:08 +0100

    'new'

--- bank_project/settings.py
+++ bank_project/settings.py
@@ -26,9 +26,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bank-app-44mt.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*.onrender.com', 'localhost', '127.0.0.1']
+
 
 ALLOWED_HOSTS = ['*']
+
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 
 # Application definition

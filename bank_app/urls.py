b9471e37759f363c8349cfac073cddb699677404
ola <atobateleameer@gmail.com>
2024-11-27 11:29:13 +0100

    'new7'

--- bank_app/urls.py
+++ bank_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 from .views import dashboard 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -8,4 +10,4 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),  # Ensure this line exists
     path('logout/', views.logout_view, name='logout'),
     path('dashboard/', dashboard, name='dashboard'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
